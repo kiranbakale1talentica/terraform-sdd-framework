47597253888c247fa2177d6da5ce1bc748fb33e1
kiranbakale1talentica <kiran.bakale@talentica.com>
2026-08-06 15:50:56 +0530

    fix(backend): configure S3 remote state backend and non-interactive terraform init

--- terraform/services/first-s3-bucket/dev/backend.tf
+++ terraform/services/first-s3-bucket/dev/backend.tf
@@ -1,3 +1,9 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket       = "terraform-sdd-tfstate-682563173581"
+    key          = "services/first-s3-bucket/dev/terraform.tfstate"
+    region       = "ap-south-1"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
